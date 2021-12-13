aa0a37bd3c415cfc3f6d086ff7c17b5c61342fc6
alok <alksbd9@gmail.com>
2021-12-13 23:33:23 +0545

    Update ARPlacerViewControllerComposer to add distance feature to app

--- ARPlacer/ARPlacer/ARPlacerViewControllerComposer.swift
+++ ARPlacer/ARPlacer/ARPlacerViewControllerComposer.swift
@@ -12,22 +12,18 @@ public class ARPlacerViewControllerComposer {
     public static func composeWith(objectPlacer: ObjectPlacer, sceneView: ARSCNView = ARSCNView()) -> ARPlacerViewController {
         let controller = ARPlacerViewControllerFromStoryBoard()
         
-        let delegate = ARPlacerSessionDelegateWithPresenterAdapter(delegate: ARPlacerSessionDelegate(), presenter: ARPlacerInformationPresenter(informationView: controller, errorView: controller))
+        let objectDistanceController = ObjectDistanceController(sceneView: sceneView)
+        let presenter = ARPlacerInformationPresenter(informationView: controller, errorView: controller)
+        let sceneDelegateWithPresenter = ARPlacerSessionDelegateWithPresenterAdapter(delegate: ARPlacerSessionDelegate(), presenter: presenter)
+        let sceneViewDelegate = ARPlacerSessionDelegateWithObjetDistanceControllerAdapter(delegate: sceneDelegateWithPresenter, controller: objectDistanceController)
         
-        sceneView.delegate = delegate
-        controller.setSceneView = { view in
-            let v = sceneView
-            v.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(v)
-            NSLayoutConstraint.activate([
-                v.topAnchor.constraint(equalTo: view.topAnchor),
-                v.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                v.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                v.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
-        }
+        sceneView.delegate = sceneViewDelegate
+        injectSceneView(sceneView, toController: controller)
         
         let arPlacerSceneViewController = ARPlacerSceneViewController(sceneView: sceneView, objectPlacer: objectPlacer)
+        let arPlacerSceneViewControllerDelegate = ARPlacerSceneViewControllerDelegateImpl(controller: objectDistanceController, presenter: presenter)
+        arPlacerSceneViewController.delegate = arPlacerSceneViewControllerDelegate
+        
         controller.sceneViewController = arPlacerSceneViewController
         return controller
     }
@@ -37,5 +33,18 @@ public class ARPlacerViewControllerComposer {
         let storyboard = UIStoryboard(name: "ARPlacer", bundle: bundle)
         let controller = storyboard.instantiateInitialViewController() as! ARPlacerViewController
         return controller
+    }
+    
+    private static func injectSceneView(_ sceneView: ARSCNView, toController controller: ARPlacerViewController) {
+        controller.setSceneView = { view in
+            sceneView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(sceneView)
+            NSLayoutConstraint.activate([
+                sceneView.topAnchor.constraint(equalTo: view.topAnchor),
+                sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        }
     }
 }
